9e4c8c713f3a014346370f20d52fcb80a8b80caf
TheJediCode <68616265+TheJediCode@users.noreply.github.com>
2020-09-03 22:12:21 -0400

    Imported header component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './header';
+import "./style.css";
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
